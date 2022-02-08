67bd086026c0c7c1a37a30d86e4d417236a3b40e
Hridoy <mdhidoyhassan@gmail.com>
2022-02-08 09:55:49 +0600

    php

---
+++ MID_LAB_TASK_04_PHP/Task_1.php
@@ -0,0 +1,7 @@
+<?php  
+    $length = 2;  
+    $width = 3;
+    $perimeter = 2*$length*$width;  
+    echo "area of rectangle is $length * $width= " . ($length * $width) . "\n"; 
+    echo "perimeter of rectangle in = $perimeter" 
+?>  
---
+++ MID_LAB_TASK_04_PHP/Task_2.php
@@ -0,0 +1,7 @@
+<?php
+    $vat = 15;
+    $price = 100;
+    $vatPrice = ($price / 100) * $vat;
+    //$totalPrice = $price + $vatPrice;
+    echo "$vatPrice Taka VAT for $price taka"
+?>
